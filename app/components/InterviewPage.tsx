a94353232d7fb569acfcb937c3f9784d5424452c
atul030702 <singhatul0307@gmail.com>
2025-06-19 20:28:32 +0530

    fixed the mic/video icons and their background with change in state

--- app/components/InterviewPage.tsx
+++ app/components/InterviewPage.tsx
@@ -1,48 +1,75 @@
+"use client"
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 const InterviewPage = () => {
-  return (
-    <div className="w-full h-screen flex justify-center items-center p-10">
-      <div className="w-[70%] h-full mr-2.5 flex flex-col justify-between">
-        <div className="w-full aspect-video text-center rounded-sm bg-gray-300">
-          Panel Video/Audio
-        </div>
-        <div className="w-full flex justify-between items-center">
-          <div className="w-1/6 aspect-video text-center rounded-sm bg-gray-300">
-            User Video
-          </div>
-          <div className="w-1/6 h-full flex flex-col justify-end">
-            <div className="w-full flex justify-between items-center mb-2.5">
-              <button className="bg-black">
-                <Image
-                  src={"/mic_off.svg"}
-                  alt="Mic Icon"
-                  width={20}
-                  height={20}
-                  priority
-                />
-              </button>
-              <button className="bg-black">
-                <Image
-                  src={"/videocam_on.svg"}
-                  alt="Videocam Icon"
-                  width={20}
-                  height={20}
-                  priority
-                />
-              </button>
+    const [isMicOn, setIsMicOn] = useState(true);
+    const [isVideoOn, setIsVideoOn] = useState(true);
+
+    const toggleMic = () => {
+        setIsMicOn((previous) => !previous);
+    };
+
+    const toggleVideo = () => {
+        setIsVideoOn((previous) => !previous);
+    }
+
+    return (
+        <div className="w-full h-screen flex justify-center items-center p-10">
+
+            <div className="w-[70%] h-full mr-2.5 flex flex-col justify-between">
+
+                <div className="w-full aspect-video text-center rounded-sm bg-gray-300">
+                    Panel Video/Audio
+                </div>
+
+                <div className="w-full flex justify-between items-center">
+
+                    <div className="w-1/6 aspect-video text-center rounded-sm bg-gray-300">
+                        User Video
+                    </div>
+
+                    <div className="w-1/6 h-full flex flex-col justify-end whitespace-nowrap">
+
+                        <div className="w-full flex justify-between items-center mb-2.5">
+
+                            <button onClick={toggleMic} 
+                                className={`p-2 rounded-full cursor-pointer transition ${isMicOn ? "bg-green-700 hover:bg-green-700/90" : "bg-red-500 hover:bg-red-500/90"}`}
+                            >
+                                <Image
+                                    src={isMicOn ? "/mic_on.svg" : "/mic_off.svg"}
+                                    alt="Mic Icon"
+                                    width={30}
+                                    height={30}
+                                    priority
+                                />
+                            </button>
+
+                            <button onClick={toggleVideo} 
+                                className={`p-2 rounded-full cursor-pointer transition ${isVideoOn ? "bg-green-700 hover:bg-green-700/90" : "bg-red-500 hover:bg-red-500/90"}`}
+                            >
+                                <Image
+                                    src={isVideoOn ? "/videocam_on.svg" : "/videocam_off.svg"}
+                                    alt="Videocam Icon"
+                                    width={30}
+                                    height={30}
+                                    priority
+                                />
+                            </button>
+
+                        </div>
+
+                        <button className="w-full py-2 px-2.5 rounded-sm cursor-pointer text-white bg-red-500 hover:bg-red-500/90 transition">
+                            End Interview
+                        </button>
+                    </div>
+                </div>
             </div>
-            <button className="w-full py-2 px-2.5 rounded-sm cursor-pointer text-white bg-red-500">
-              End Meeting
-            </button>
-          </div>
+            <div className="w-[25%] h-full ml-2.5 text-center rounded-sm bg-gray-300">
+                Transcripts
+            </div>
         </div>
-      </div>
-      <div className="w-[25%] h-full ml-2.5 text-center rounded-sm bg-gray-300">
-        Transcripts
-      </div>
-    </div>
-  );
+    );
 };
 
 export default InterviewPage;
